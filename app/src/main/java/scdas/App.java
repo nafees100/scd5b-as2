3b5c19f5b8b309a8fe52c370b9d8653c94020314
Nafees Hassan <nafeeshassan16@gmail.com>
2023-10-09 23:45:00 +0500

    Added stream test

--- app/src/main/java/scdas/App.java
+++ app/src/main/java/scdas/App.java
@@ -3,10 +3,24 @@ package scdas;
 import java.util.*;
 
 import stack.*;
+import stream.*;
 
 public class App {
 	public String getGreeting() {
 		return "Hello World!";
+	}
+
+	private static void streamTest(){
+		FirstNonRepeatingStream stream = new FirstNonRepeatingStream();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a line full of characrters:");
+		String line = scanner.next();
+		byte[] bytes = line.getBytes();
+		for (int i = 0; i < bytes.length; i ++)
+			stream.add((char)bytes[i]);
+		System.out.println("first non repeating character: " +
+				stream.getFirstNonRepeating());
+		scanner.close();
 	}
 
 	public static void main(String[] args) {
@@ -19,6 +33,9 @@ public class App {
 				break;
 			case 1:
 				stack.Demo.stackMain();
+				break;
+			case 2:
+				streamTest();
 				break;
 			default:
 				System.out.println("Invalid choice.");
